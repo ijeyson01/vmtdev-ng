06be392e371b224b52cb1dba89f698020899b32d
Jeyson Jaramillo <jjaramillo@viamatica.com>
2025-02-01 13:46:15 -0500

    Introduccion a componentes compartidos

--- src/app/modules/crud/crud.module.ts
+++ src/app/modules/crud/crud.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PersonComponent } from './person/person.component';
 import { DeviceComponent } from './device/device.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 
@@ -11,7 +12,8 @@ import { DeviceComponent } from './device/device.component';
     DeviceComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   exports: [
     PersonComponent,
--- src/app/modules/shared/table-button/table-button.component.ts
+++ src/app/modules/shared/table-button/table-button.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { OutletContext } from '@angular/router';
 
 @Component({
   selector: 'app-table-button',
@@ -6,5 +7,50 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class TableButtonComponent {
+
+  @Input()
+  labelUpdate: string = 'Update';
+
+  @Input()
+  labelDelete: string = 'Delete';
+
+  @Input()
+  labelAditional: string = 'Option';
+
+  @Input()
+  showUpdate: boolean = true;
+
+  @Input()
+  showDelete: boolean = true;
+
+  @Input()
+  showOption: boolean = false;
+
+  @Input()
+  styleButtonUpdate: string = 'primary';
+
+  @Input()
+  styleButtonDelete: string = 'danger';
+
+  @Input()
+  styleButtonOption: string = 'success'
+
+  @Output() eventUpdate = new EventEmitter();
+
+  @Output() eventDelete = new EventEmitter();
+
+  @Output() eventOption = new EventEmitter();
+
+  clicEventUpdate() {
+    this.eventUpdate.emit();
+  }
+
+  clicEventDelete() {
+    this.eventDelete.emit();
+  }
+
+  clicEventOption() {
+    this.eventOption.emit();
+  }
 
 }
