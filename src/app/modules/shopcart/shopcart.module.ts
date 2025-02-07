7e3bb331d7a386fa02650f6c35f78f3a7063455e
Jeyson Jaramillo <jjaramillo@viamatica.com>
2025-02-06 19:45:04 -0500

    Componente productos

--- src/app/modules/shopcart/shopcart.module.ts
+++ src/app/modules/shopcart/shopcart.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TableProductComponent } from './table-product/table-product.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 
@@ -9,7 +10,8 @@ import { TableProductComponent } from './table-product/table-product.component';
     TableProductComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   exports: [
     TableProductComponent
