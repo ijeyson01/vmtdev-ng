350d60a888a70b5c515ea7faf88e046d513e3d24
Jeyson Jaramillo <jjaramillo@viamatica.com>
2025-02-06 20:56:56 -0500

    Interaccion entre tablas de carrito v1

--- src/app/modules/shopcart/product-cart/product-cart.component.ts
+++ src/app/modules/shopcart/product-cart/product-cart.component.ts
@@ -12,7 +12,41 @@ export class ProductCartComponent {
   @ViewChild(TableProductSelectedComponent) productSelected!: TableProductSelectedComponent;
 
   aggProduct(product: ProductDetailI) {
-    this.productSelected.listProductSelected.push(product);
+    let productSelected = product.product;
+    let productCategorySelected = product.category;
+    let productPriceSelected = product.price;
+    let productIdSelected = product.id;
+
+    if(this.productSelected.listProductSelected.length > 0) {
+      let productValidation = this.productSelected.listProductSelected.find( productInCart => productInCart.id == productIdSelected);
+      if(productValidation) {
+        this.productSelected.listProductSelected.map( productInCart => {
+          if(productInCart.id == productValidation.id) {
+            productInCart.stock++;
+          }
+        })
+      } else {
+        let newProduct: ProductDetailI = {
+          id: productIdSelected,
+          product: productSelected,
+          category: productCategorySelected,
+          price: productPriceSelected, 
+          provider: '',
+          stock: 1
+        } 
+        this.productSelected.listProductSelected.push(newProduct);
+      }
+    } else {
+      let newProduct: ProductDetailI = {
+        id: productIdSelected,
+        product: productSelected,
+        category: productCategorySelected,
+        price: productPriceSelected, 
+        provider: '',
+        stock: 1
+      } 
+      this.productSelected.listProductSelected.push(newProduct);
+    }
   }
 
 }
