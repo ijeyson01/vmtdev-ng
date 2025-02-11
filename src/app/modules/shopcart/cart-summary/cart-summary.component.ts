84d8f011be6591b2c2812b6ace15600afef1d5eb
Jeyson Jaramillo <jjaramillo@viamatica.com>
2025-02-10 19:32:50 -0500

    Resumen de compra

--- src/app/modules/shopcart/cart-summary/cart-summary.component.ts
+++ src/app/modules/shopcart/cart-summary/cart-summary.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class CartSummaryComponent {
-
+  subtotal: number = 0;
+  iva: number = this.subtotal * 0.15;
+  subtotaliva: number = this.subtotal + this.iva;
+  valorDescuento: number = 0;
+  totalDescuento: number = this.subtotaliva * this.valorDescuento;
+  totalPago: number = this.subtotaliva - this.totalDescuento;
 }
